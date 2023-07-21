5758354dfbd4b70a4b50d6f4ae4009917fd0699a
Adrian Mohnacs <amohnacs@gmail.com>
2023-07-21 16:00:00 +0700

    Continued regression upload

--- music_pipeline/regression.py
+++ music_pipeline/regression.py
@@ -1,4 +1,8 @@
 import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge
+import matplotlib.pyplot as plt
+from sklearn.linear_model import Lasso
 
 # Create X from the radio column's values
 X = sales_df["radio"].values
@@ -12,9 +16,6 @@ X = X.reshape(-1, 1)
 # Check the shape of the features and targets
 print(X.shape, y.shape)
 
-# Import LinearRegression
-from sklearn.linear_model import LinearRegression
-
 # Create the model
 reg = LinearRegression()
 
@@ -26,9 +27,6 @@ predictions = reg.predict(X)
 
 print(predictions[:5])
 
-# Import matplotlib.pyplot
-import matplotlib.pyplot as plt
-
 # Create scatter plot
 plt.scatter(X, y, color="blue")
 
@@ -38,4 +36,43 @@ plt.xlabel("Radio Expenditure ($)")
 plt.ylabel("Sales ($)")
 
 # Display the plot
+plt.show()
+
+# Print the mean
+print(np.mean(cv_results))
+
+# Print the standard deviation
+print(np.std(cv_results))
+
+# Print the 95% confidence interval
+print(np.quantile( cv_results, [0.25, 0.75]))
+
+alphas = [0.1, 1.0, 10.0, 100.0, 1000.0, 10000.0]
+ridge_scores = []
+
+for alpha in alphas:
+  
+  # Create a Ridge regression model
+  ridge = Ridge(alpha=alpha)
+  
+  # Fit the data
+  ridge.fit(X_train, y_train)
+  
+  # Obtain R-squared
+  score = ridge.score(X_test, y_test)
+  ridge_scores.append(score)
+
+print(ridge_scores)
+
+# Instantiate a lasso regression model
+lasso = Lasso(alpha=0.3)
+
+# Fit the model to the data
+fitter = lasso.fit(X, y)
+
+# Compute and print the coefficients
+lasso_coef = fitter.coef_
+print(lasso_coef)
+plt.bar(sales_columns, lasso_coef)
+plt.xticks(rotation=45)
 plt.show()
